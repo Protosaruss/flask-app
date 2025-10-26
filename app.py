9b4d2fc0086f5b2bc668f8dbd34779f2c37c86c6
Protosarus <128179752+Protosaruss@users.noreply.github.com>
2025-10-26 22:36:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Merhaba! Render üzerinde çalışan ilk Flask uygulamam 🎉"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
